69373fd9030f2e90ade889191ff30af9962cae33
François Donzaud <francois.donzaudpro@gmail.com>
2025-01-09 12:39:20 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 // Game variables
 let player;
+let floor;
 
 function setup() {
     // Create the game canvas (16:9 aspect ratio)
-    createCanvas(1280, 720);
+    createCanvas(windowWidth, windowHeight);
 
     // Create the player sprite
     player = new Sprite();
@@ -14,26 +15,48 @@ function setup() {
     // Set up basic player movement
     player.friction = 0.1;
     player.maxSpeed = 4;
+
+    world.gravity = 5;
+
+    floor = new Sprite();
+    floor.width = windowWidth;
+    floor.height = 100;
+    floor.y = height - floor.height / 2;
+    floor.color = 'green';
+    floor.immovable = true;
+    floor.collider = 'static';
 }
 
 function draw() {
     // Set the background
     background(220);
 
-    // Player movement controls
-    if (kb.pressing('left')) {
-        player.vel.x = -2;
-    }
-    if (kb.pressing('right')) {
-        player.vel.x = 2;
-    }
-    if (kb.pressing('up')) {
-        player.vel.y = -2;
-    }
-    if (kb.pressing('down')) {
-        player.vel.y = 2;
+    // Update the player
+    player.update();
+
+    // move the camera to follow the player
+    camera.position.x = player.x;
+    camera.position.y = player.y;
+
+    // controll of the player
+    
+
+    // Player jump control
+    if (kb.presses('space') && player.collides(floor)) {
+        player.vel.y = -10;
     }
 
-    // In p5play v3, we don't need drawSprites() anymore
-    // Sprites are automatically drawn
+    // Shooting control
+    if (kb.presses('f')) {
+        let bullet = new Sprite(player.x, player.y, 10, 10);
+        bullet.color = 'red';
+        bullet.vel.x = player.mirror.x ? -10 : 10;
+    }
+
+    // Update player direction based on movement
+    if (player.vel.x > 0) {
+        player.mirror.x = false; // Facing right
+    } else if (player.vel.x < 0) {
+        player.mirror.x = true; // Facing left
+    }
 } 
